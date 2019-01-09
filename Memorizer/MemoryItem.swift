ecd8496112b81b985fb306d0fbce95a91bae177e
emin.roblack@gmail.com <emin.roblack@gmail.com>
2019-01-09 13:09:02 +0100

    Add items

---
+++ Memorizer/MemoryItem.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryItem.swift
+//  Memorizer
+//
+//  Created by Emin Roblack on 1/9/19.
+//  Copyright © 2019 emiN Roblack. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryItem: Codable {
+  var title: String
+  var text: String
+}
